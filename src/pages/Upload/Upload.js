b684b4a9698527702aa93c395f5c9453cf1045a6
tatianapriv <severny.island@gmail.com>
2023-03-06 16:57:09 -0800

    style the mobile version of the Upload page

--- src/pages/Upload/Upload.js
+++ src/pages/Upload/Upload.js
@@ -10,42 +10,45 @@ function Upload() {
       <Header />
       <section className="form">
         <h1 className="form__title">Upload Video</h1>
+        <div className="form__container">
+          <label className="form__subtitle">Video Thumbnail</label>
+          <img
+            src={UploadVideo}
+            className="form__thumbnail"
+            alt="thumbnail"
+          ></img>
 
-        <label className="form__subtitle">Video Thumbnail</label>
-        <img src={UploadVideo} className="form__thumbnail" alt="thumbnail"></img>
-    
-      
-      <form id="form__form" className="form__form">
-        <label htmlFor="name" className="form__label--title">
-          Title your video
-        </label>
-        <input
-          id="name"
-          className="form__input--title"
-          type="text"
-          placeholder="Add a title to your video"
-          name="videoTitle"
-        />
+          <form id="form__form" className="form__form">
+            <label htmlFor="name" className="form__label--title">
+              Title your video
+            </label>
+            <input
+              id="name"
+              className="form__input--title"
+              type="text"
+              placeholder="Add a title to your video"
+              name="videoTitle"
+            />
 
-        <label htmlFor="description" className="form__label--description">
-          Add a video description
-        </label>
-        <textarea
-          id="description"
-          className="form__input--description"
-          placeholder="Add a description to your video"
-          rows="5"
-          cols="30"
-         
-          name="videoDescription"
-        ></textarea>
-      </form>
-      <Button className="btn--publish" text="Publish" link="/" />
-      <Button className="btn--cancel" text="Cancel" link="/"/>
-   </section>
-   
+            <label htmlFor="description" className="form__label--description">
+              Add a video description
+            </label>
+            <textarea
+              id="description"
+              className="form__input--description"
+              placeholder="Add a description to your video"
+              rows="5"
+              cols="30"
+              name="videoDescription"
+            ></textarea>
+            <hr />
+            <Button className="btn--publish" text="Publish" link="/" />
+            <Button className="btn--cancel" text="Cancel" link="/" />
+          </form>
+        </div>
+      </section>
     </>
-  )
+  );
 }
 
 export default Upload;
