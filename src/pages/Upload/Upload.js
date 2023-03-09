b47e5919dc30b12c08d99e468024ccdda39a71b6
tatianapriv <severny.island@gmail.com>
2023-03-08 16:52:09 -0800

    fix css for the Upload page

--- src/pages/Upload/Upload.js
+++ src/pages/Upload/Upload.js
@@ -35,46 +35,52 @@ function Upload() {
       <section className="form">
         <h1 className="form__title">Upload Video</h1>
         <div className="form__container">
-          
           <form
             onSubmit={(event) => handleSubmit(event)}
             id="form__form"
             className="form__form"
           >
-            <div className="form__box">
-            <label className="form__subtitle">Video Thumbnail</label>
-            <img
-              src={UploadVideo}
-              className="form__thumbnail"
-              alt="thumbnail"
-            ></img>
-          </div>
-            <label htmlFor="name" className="form__label--title">
-              Title your video{" "}
-            </label>
-            <input
-              id="name"
-              className="form__input--title"
-              type="text"
-              placeholder="Add a title to your video"
-              name="videoTitle"
-              onChange={handleChangeTitle}
-              value={videoTitle}
-            />
+            <div className="form__info">
+              <div className="form__box">
+                <label className="form__subtitle">Video Thumbnail</label>
+                <img
+                  src={UploadVideo}
+                  className="form__thumbnail"
+                  alt="thumbnail"
+                ></img>
+              </div>
+              <div className="form__wrapper">
+                <label htmlFor="name" className="form__label--title">
+                  Title your video{" "}
+                </label>
+                <input
+                  id="name"
+                  className="form__input--title"
+                  type="text"
+                  placeholder="Add a title to your video"
+                  name="videoTitle"
+                  onChange={handleChangeTitle}
+                  value={videoTitle}
+                />
 
-            <label htmlFor="description" className="form__label--description">
-              Add a video description{" "}
-            </label>
-            <textarea
-              id="description"
-              className="form__input--description"
-              placeholder="Add a description to your video"
-              rows="5"
-              cols="30"
-              name="videoDescription"
-              onChange={handleChangeDescription}
-              value={videoDescription}
-            ></textarea>
+                <label
+                  htmlFor="description"
+                  className="form__label--description"
+                >
+                  Add a video description{" "}
+                </label>
+                <textarea
+                  id="description"
+                  className="form__input--description"
+                  placeholder="Add a description to your video"
+                  rows="5"
+                  cols="30"
+                  name="videoDescription"
+                  onChange={handleChangeDescription}
+                  value={videoDescription}
+                ></textarea>
+              </div>
+            </div>
             {/* <button className="btn--publish" type="submit">
               Publish
             </button> */}
